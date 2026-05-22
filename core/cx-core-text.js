fc340dcd0a1bd7cbcb085f87fab5b264e5d876ba
ossmcloud <development@ossmcloud.ie>
2026-05-22 14:22:36 +0100

    base 64 decode encoding

--- core/cx-core-text.js
+++ core/cx-core-text.js
@@ -8,10 +8,10 @@ function trimRight(str, char) {
     return str.substring(0, str.length - 1);
 }
 
-function decodeBase64String(base64String) {
+function decodeBase64String(base64String, encoding) {
     if (!base64String) { return ''; }
     var buff = Buffer.from(base64String, 'base64');
-    return buff.toString('ascii');
+    return buff.toString(encoding || 'ascii');
 }
 
 function encodeBase64String(str) {
@@ -30,10 +30,10 @@ function fromCamelCase(camelCasingString) {
     if (!camelCasingString) { return ''; }
     camelCasingString = camelCasingString.replaceAll('_', ' ').trim();
 
-    
+
     var parsedString = camelCasingString[0].toLowerCase();
     for (var cx = 1; cx < camelCasingString.length; cx++) {
-        
+
         if (camelCasingString[cx].toUpperCase() == camelCasingString[cx]) {
             // check if prev letter is upper case
             if (camelCasingString[cx - 1].toUpperCase() == camelCasingString[cx - 1]) {
