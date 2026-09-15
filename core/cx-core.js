5cb114d219d92582e8c924f298f688d8d0835f5b
ossmcloud <development@ossmcloud.ie>
2026-09-15 07:58:18 +0100

    approval

--- core/cx-core.js
+++ core/cx-core.js
@@ -40,7 +40,57 @@ function bufferToString(buf) {
     return `0x${buf.toString('hex')}`
 }
 
+//
+// @@NOTE: this function returns an array of property names from a class
+//         the key here is Object.getOwnPropertyNames(Object.getPrototypeOf(class))
+//         we do it recursively to handle classes as properties
+// 
+function getClassMethodNames(klass, methodType) {
+    const isGetter = (x, name) => (Object.getOwnPropertyDescriptor(x, name) || {}).get;
+    const isFunction = (x, name) => typeof x[name] === 'function';
+    const deepFunctions = x =>
+        x !== Object.prototype &&
+        Object.getOwnPropertyNames(x)
+            .filter(name => {
+                if (!methodType || methodType == 'property') { return isGetter(x, name); }
+                if (methodType == 'function') { return isFunction(x, name); }
+                return false;
+            }).concat(deepFunctions(Object.getPrototypeOf(x)) || []);
+    const distinctDeepFunctions = klass => Array.from(new Set(deepFunctions(klass)));
+    const allMethods = typeof klass.prototype === "undefined" ? distinctDeepFunctions(klass) : Object.getOwnPropertyNames(klass.prototype);
+    return allMethods.filter(name => name !== 'constructor' && !name.startsWith('__'))
+}
+//
+// Classes can be serialized but members that are private won't appear, and that's because members in classes are actually functions so JSON.stringify will ignore them
+// This function converts a class to a Json object so it can be serialized fine
+//
+function classToObject(c) {
+    var o = {};
+    var propertyNames = getClassMethodNames(c);
+    for (var px = 0; px < propertyNames.length; px++) {
+        var k = propertyNames[px];
+        if (isObject(c[k])) {
+            o[k] = classToObject(c[k]);
+        } else if (Array.isArray(c[k])) {
+            o[k] = [];
+            for (var x = 0; x < c[k].length; x++) {
+                o[k].push(classToObject(c[k][x]));
+            }
+        } else {
+            o[k] = c[k];
+        }
+    }
+    return o;
+}
 
+function arrayToObject(a) {
+    if (!a) { return null; }
+    var res = [];
+    for (var ax = 0; ax < a.length; ax++){
+        res.push(classToObject(a[ax]));
+    }
+    return res;
+}
 
 module.exports = {
     getAllKeys: getAllKeys,
@@ -49,4 +99,6 @@ module.exports = {
     isFunc: isFunction,
     isObj: isObject,
     bufferToString: bufferToString,
+    classToObject: classToObject,
+    arrayToObject: arrayToObject,
 }
